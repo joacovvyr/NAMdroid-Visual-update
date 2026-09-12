2e860cd8e4b6efb0f3f57049076af6462514caf4
joacovvyr <joacovvyrmi@gmail.com>
2026-09-12 15:08:03 -0300

    Fix animation import in Droid editor family

--- app/src/main/java/com/namdroid/app/ui/StandardDroidEditors.kt
+++ app/src/main/java/com/namdroid/app/ui/StandardDroidEditors.kt
@@ -3,6 +3,7 @@ package com.namdroid.app.ui
 import androidx.compose.animation.core.FastOutSlowInEasing
 import androidx.compose.animation.core.RepeatMode
 import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.rememberInfiniteTransition
